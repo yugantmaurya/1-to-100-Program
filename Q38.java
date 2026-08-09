0bdc5161a2c09d12c9a25fdb540051db0aef25f0
yugantmaurya <yugantmaurya1@gmail.com>
2026-08-09 09:54:26 +0530

    added new progranm

---
+++ Q38.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Q38 {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      String a, b = "";
+
+      System.out.println("Enter The Strings :");
+      a = sc.nextLine();
+
+      for (int i = 0; i < a.length(); i++) {
+
+         char ch = a.charAt(i);
+
+         if (ch >= 'a' && ch <= 'z') {
+            ch = (char) (ch - 32);
+         }
+
+         b = b + ch;
+      }
+
+      System.out.println("Uppercase: " + b);
+
+   }
+}

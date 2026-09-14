3f9b35ea9bfb17976f179c1c02e98057e7588120
yugantmaurya <yugantmaurya1@gmail.com>
2026-09-14 11:07:14 +0530

    Added New Program

---
+++ 49. Recursive binary search/Q49.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Q49 {
+
+    static int binarySearch(int[] arr, int low, int high, int target) {
+
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (target < arr[mid]) {
+            return binarySearch(arr, low, mid - 1, target);
+        }
+
+        return binarySearch(arr, mid + 1, high, target);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements in sorted order:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter element to search: ");
+        int target = sc.nextInt();
+
+        int result = binarySearch(arr, 0, n - 1, target);
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+
+        sc.close();
+    }
+}
